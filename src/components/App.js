a73717fb85937562930473302e92c44c0612e9d1
dnlengnl12 <70835908+dnlengnl12@users.noreply.github.com>
2021-10-06 23:22:53 +0900

    Updated AuthService

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,15 @@
-import React from "react";
-import AppRouter from "./Router";
+import {React, useState} from "react";
+import AppRouter from "components/Router";
+import { authService } from "fbase";
 
 function App() {
+  console.log(authService.currentUser);
+  const [isLoggedIn, setIsLoggedIn] = useState(authService.currentUser);
   return (
-    <AppRouter></AppRouter>
+    <>
+      <AppRouter isLoggedIn={isLoggedIn}></AppRouter>
+      <footer>&copy; Twitter {new Date().getFullYear()}</footer>
+    </>
   );
 }
 
--- src/routes/Auth.js
+++ src/routes/Auth.js
@@ -1,4 +1,6 @@
 import React from "react";
+import AppRouter from "components/Router";
+import fbase from "fbase";
 
 const Auth = () => <span>Auth</span>;
 export default Auth;
