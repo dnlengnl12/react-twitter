24f8a50732721d9b863f5ad38e316b9b0ea5134d
dnlengnl12 <70835908+dnlengnl12@users.noreply.github.com>
2021-10-17 23:11:53 +0900

    Updated function Read

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,9 +1,24 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { dbService } from "fbase";
-import { addDoc, collection } from "firebase/firestore";
+import { addDoc, collection, getDocs, query } from "firebase/firestore";
 
 const Home = () => {
     const [tweet, setTweet] = useState("");
+    const [tweets, setTweets] = useState([]);
+    const getTweets = async () => {
+        const q = query(collection(dbService, "tweets"));
+        const querySnapshot = await getDocs(q);
+        querySnapshot.forEach((doc) => {
+            const tweetObj = {
+                ...doc.data(),
+                id:doc.id,
+            }
+            setTweets(prev => [tweetObj, ...prev]);
+        });
+    };
+    useEffect(() => {
+        getTweets();
+    }, []);
     const onSubmit = async (event) => {
         event.preventDefault();
 
@@ -28,6 +43,11 @@ const Home = () => {
                 <input value={tweet} onChange={onChange}type="text" placeholder="what's on your mind" maxLength={120} />
                 <input type="submit" value="Tweet" />
             </form>
+            <div>
+                {tweets.map(tweet => <div key={tweet.id}>
+                    <h4>{tweet.tweet}</h4>
+                    </div>)}
+            </div>
         </div>
     );
 };
