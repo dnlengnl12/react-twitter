595d18ffa052fcf0f11339cba51cb9857c851487
dnlengnl12 <70835908+dnlengnl12@users.noreply.github.com>
2021-10-21 23:33:11 +0900

    Updated Delete

--- src/components/Tweet.js
+++ src/components/Tweet.js
@@ -1,15 +1,24 @@
 import React from "react";
 
-const Tweet = ({ tweetObj, isOwner }) => (
-    <div key={tweetObj.id}>
-        <h4>{tweetObj.text}</h4>
-        {isOwner && (
-            <>
-                <button>Delete Tweet</button>
-                <button>Edit Tweet</button>
-            </>
-        )}
-    </div>
-);
+const Tweet = ({ tweetObj, isOwner }) => {
+    const onDeleteClick = () => {
+        const ok = confirm("Are you sure you want to delete this tweet");
+        if(ok) {
+            await deleteDoc(TweetTextRef);
+            //delete
+        }
+    }
+    return (
+        <div key={tweetObj.id}>
+            <h4>{tweetObj.text}</h4>
+            {isOwner && (
+                <>
+                    <button onClick={onDeleteClick}>Delete Tweet</button>
+                    <button>Edit Tweet</button>
+                </>
+            )}
+        </div>
+    );
+}
 
 export default Tweet;
