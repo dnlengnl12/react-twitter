46ac330868c21d3927a7f75d5a409f6229189814
dnlengnl12 <70835908+dnlengnl12@users.noreply.github.com>
2021-10-22 23:39:01 +0900

    Updated text update

--- src/components/Tweet.js
+++ src/components/Tweet.js
@@ -1,24 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
+import { doc, deleteDoc } from "firebase/firestore";
+import { dbService } from "fbase";
 
 const Tweet = ({ tweetObj, isOwner }) => {
-    const onDeleteClick = () => {
-        const ok = confirm("Are you sure you want to delete this tweet");
+    const [editing, setEditing] = useState(false);
+    const [newTweet, setNewTeet] = useState(tweetObj.text);
+    const onDeleteClick = async () => {
+        const ok = window.confirm("Are you sure you want to delete this tweet");
         if(ok) {
+            const TweetTextRef = doc(dbService, "tweets", `${tweetObj.id}`);
             await deleteDoc(TweetTextRef);
             //delete
         }
-    }
+    };
+    const toggleEditing = () => setEditing((prev) => !prev);
     return (
-        <div key={tweetObj.id}>
-            <h4>{tweetObj.text}</h4>
+        <div>
+            {editing ? (
+                <>
+                    <form>
+                        <input value={newTweet} required/>
+                    </form>
+                    <button onClick={toggleEditing}>cancel</button>
+                </>
+             ) : (
+            <>
             {isOwner && (
                 <>
                     <button onClick={onDeleteClick}>Delete Tweet</button>
-                    <button>Edit Tweet</button>
+                    <button onClick={toggleEditing}>Edit Tweet</button>
                 </>
             )}
+            </>
+        )};
         </div>
-    );
+    )
 }
 
 export default Tweet;
