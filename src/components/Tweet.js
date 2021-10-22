bc69770b07086d275dcbf436f67fd9e51ed817a9
dnlengnl12 <70835908+dnlengnl12@users.noreply.github.com>
2021-10-23 00:22:27 +0900

    Updated edit function

--- src/components/Tweet.js
+++ src/components/Tweet.js
@@ -1,10 +1,10 @@
 import React, { useState } from "react";
-import { doc, deleteDoc } from "firebase/firestore";
+import { doc, deleteDoc, updateDoc } from "firebase/firestore";
 import { dbService } from "fbase";
 
 const Tweet = ({ tweetObj, isOwner }) => {
     const [editing, setEditing] = useState(false);
-    const [newTweet, setNewTeet] = useState(tweetObj.text);
+    const [newTweet, setNewTweet] = useState(tweetObj.text);
     const onDeleteClick = async () => {
         const ok = window.confirm("Are you sure you want to delete this tweet");
         if(ok) {
@@ -14,27 +14,50 @@ const Tweet = ({ tweetObj, isOwner }) => {
         }
     };
     const toggleEditing = () => setEditing((prev) => !prev);
+    const onSubmit = async (event) => {
+        event.preventDefault();
+        const TweetTextRef = doc(dbService, "tweets", `${tweetObj.id}`);
+        await updateDoc(TweetTextRef, {
+            text: newTweet,
+        });
+        setEditing(false);
+    }
+    const onChange = (event) => {
+        const {
+            target:{value},
+        } = event;
+        setNewTweet(value);
+    };
     return (
         <div>
             {editing ? (
                 <>
-                    <form>
-                        <input value={newTweet} required/>
+                    <form onSubmit={onSubmit}>
+                        <input 
+                            type="text"
+                            placeholder="Edit your Tweet"
+                            value={newTweet}
+                            required
+                            onChange={onChange}    
+                        />
+
+                        <input type="submit" value="Edit"></input>
                     </form>
                     <button onClick={toggleEditing}>cancel</button>
                 </>
-             ) : (
+            ) : (
             <>
-            {isOwner && (
-                <>
-                    <button onClick={onDeleteClick}>Delete Tweet</button>
-                    <button onClick={toggleEditing}>Edit Tweet</button>
-                </>
-            )}
+                 <h4>{tweetObj.text}</h4>
+                {isOwner && (
+                    <>
+                        <button onClick={onDeleteClick}>Delete Tweet</button>
+                        <button onClick={toggleEditing}>Edit Tweet</button>
+                    </>
+                )}
             </>
-        )};
+        )}
         </div>
-    )
+    );
 }
 
 export default Tweet;
