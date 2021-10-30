443e4a408c66c47ac5c6a29f51191121e290e63c
dnlengnl12 <70835908+dnlengnl12@users.noreply.github.com>
2021-10-30 21:03:27 +0900

    Updated Profile

--- src/components/Router.js
+++ src/components/Router.js
@@ -18,7 +18,7 @@ const AppRouter = ({ isLoggedIn, userObj }) => {
                         <Home userObj = {userObj}></Home>
                     </Route>
                     <Route exact path="/profile">
-                        <Profile></Profile>
+                        <Profile userObj={userObj}></Profile>
                     </Route>
                     <Redirect from="*" to="/" />
                 </>
--- src/routes/Profile.js
+++ src/routes/Profile.js
@@ -1,14 +1,28 @@
-import { authService } from "fbase";
-import React from "react";
+import { authService, dbService } from "fbase";
+import React, { useEffect } from "react";
 import { useHistory } from "react-router";
+import { collection, getDocs, query, where } from "firebase/firestore";
 
 
-export default () => {
+export default (userObj) => {
     const history = useHistory();
     const onLogOutClick = () => {
         authService.signOut();
         history.push("/");
     };
+    const getMyNweets = async () => {
+        const q = query(
+            collection(dbService, "nweets"),
+            where("creatorId", "==", userObj.uid)
+        );
+            const querySnapshot = await getDocs(q);
+            querySnapshot.forEach((doc) => {
+                console.log(doc.id, " => ", doc.data());
+            });
+        };
+    useEffect(() => {
+        getMyTweets();
+    }, [])
     return (
         <button onClick={onLogOutClick}>Log out</button>
     );
